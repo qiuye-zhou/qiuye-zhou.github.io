c6bd220e3988e5710f92fd7126e44ecaf0db14e2
秋叶ゞ <75529722+zhou130258@users.noreply.github.com>
2022-05-29 12:57:43 +0800

    cc

--- js/main.js
+++
@@ -1,8 +0,0 @@
-// function init() {
-//     console.log("终于可以为所欲为了");
-// }
-// //因为界面加载原因，我们延迟500ms再调用init
-// setTimeout("init()", 500)
-window.onload = function() {
-    console.log("测试head  link  引入的js文件");
-}
